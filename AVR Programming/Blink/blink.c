e40ea73032a858735aa13e74366b85cb9fb0613b
DarioSoller <dariosoller@gmx.net>
2012-10-29 10:45:28 +0100

    blink portb/pin debug

--- AVR Programming/Blink/blink.c
+++ AVR Programming/Blink/blink.c
@@ -13,15 +13,16 @@
 
 int main (void)
 {
-  //Set PORTB.3 to be output
+  //Set PORTB.3 to be output (physical pin #3 on the ATTINY85)
   DDRB = 0b00001000;
+
   while (1) {
-    //Set PORTB.3 high
+//    //Set PORTB.3 high
     PORTB = 0b00001000;
 
     _delay_ms(1000);
 
-    //Set PORTB.3 low again
+//    //Set PORTB.3 low again
     PORTB = 0b00000000;
 
     _delay_ms(1000);
